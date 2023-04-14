b40f8c1ecfbc21578af7e4e71dfb26156c2b5b1a
RiccardoGrande <grandericcardo1997@gmail.com>
2023-04-14 16:40:04 +0200

    add new img

--- src/components/AppLinks.vue
+++ src/components/AppLinks.vue
@@ -7,23 +7,27 @@ export default {
             links: [
                 {
                     text: 'DIGITAL COMICS',
-                    img: './assets/img/buy-comics-digital-comics.png'
+                    img: 'https://i0.wp.com/www.comicon.com/wp-content/uploads/2020/04/BATMAN-GOTHAM-NIGHTS-1_5e9dd8c96bfe20.09849239.jpg?resize=390%2C600'
 
                 },
                 {
-                    text: 'DC MERCHANDISE'
+                    text: 'DC MERCHANDISE',
+                    img: 'https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2020/06/Page-1.jpg'
 
                 },
                 {
-                    text: 'SUBSCRIPTION'
+                    text: 'SUBSCRIPTION',
+                    img: 'https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2020/06/Page-1.jpg'
 
                 },
                 {
-                    text: 'COMIC SHOP LOCATOR'
+                    text: 'COMIC SHOP LOCATOR',
+                    img: 'https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2020/06/Page-1.jpg'
 
                 },
                 {
-                    text: 'DC POWER VISA'
+                    text: 'DC POWER VISA',
+                    img: 'https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2020/06/Page-1.jpg'
 
                 }
             ]
