9da9779cecce809958c07c9f9f6d4c96936d68ed
RiccardoGrande <grandericcardo1997@gmail.com>
2023-04-14 17:44:01 +0200

    add info

--- src/components/AppInfo.vue
+++ src/components/AppInfo.vue
@@ -3,6 +3,69 @@ export default {
     name: 'AppInfo',
     data() {
         return {
+            info_1: [
+
+                {
+                    title: 'DC COMICS',
+                    text_1: 'Characters',
+                    text_2: 'Comics',
+                    text_3: 'Movies',
+                    text_4: 'TV',
+                    text_5: 'Games',
+                    text_6: 'Videos',
+                    text_7: 'News'
+
+
+
+                }
+            ],
+            info_2: [
+
+                {
+                    title: 'DC',
+                    text_1: 'Therm of use',
+                    text_2: 'Privacy policy (New)',
+                    text_3: 'Ad choices',
+                    text_4: 'Advertising',
+                    text_5: 'Jobs',
+                    text_6: 'Subscriptions',
+                    text_7: 'Talent workshops',
+                    text_8: 'CPSPC Certificates',
+                    text_9: 'Ratings',
+                    text_10: 'Shop help',
+                    text_11: 'Contact us',
+
+
+
+                }
+            ],
+            info_3: [
+
+                {
+                    title: 'SHOP',
+                    text_1: 'DC',
+                    text_2: 'MAD MAgazine',
+                    text_3: 'DC kids',
+                    text_4: 'DC Universe',
+                    text_5: 'DC powervisa',
+
+
+
+
+                }
+            ],
+            info_4: [
+
+                {
+                    title: 'DC COMICS',
+                    text_1: 'SHOP DC',
+                    text_2: 'SHOP DC COLLECTIBLES',
+
+
+
+
+                }
+            ]
 
         }
 
@@ -16,8 +79,58 @@ export default {
 <template>
     <div class="container">
         <div class="row">
-            <div class="col">
-                <span>info</span>
+            <div class="col" v-for="item in info_1">
+                <h4>{{ item.title }}</h4>
+                <div>{{ item.text_1 }}</div>
+                <div>{{ item.text_2 }}</div>
+                <div>{{ item.text_3 }}</div>
+                <div>{{ item.text_4 }}</div>
+                <div>{{ item.text_5 }}</div>
+                <div>{{ item.text_6 }}</div>
+                <div>{{ item.text_7 }}</div>
+
+
+            </div>
+            <div class="col" v-for="item in info_2">
+                <h4>{{ item.title }}</h4>
+                <div>{{ item.text_1 }}</div>
+                <div>{{ item.text_2 }}</div>
+                <div>{{ item.text_3 }}</div>
+                <div>{{ item.text_4 }}</div>
+                <div>{{ item.text_5 }}</div>
+                <div>{{ item.text_6 }}</div>
+                <div>{{ item.text_7 }}</div>
+                <div>{{ item.text_8 }}</div>
+                <div>{{ item.text_9 }}</div>
+                <div>{{ item.text_10 }}</div>
+
+
+            </div>
+            <div class="col" v-for="item in info_3">
+                <h4>{{ item.title }}</h4>
+                <div>{{ item.text_1 }}</div>
+                <div>{{ item.text_2 }}</div>
+                <div>{{ item.text_3 }}</div>
+                <div>{{ item.text_4 }}</div>
+                <div>{{ item.text_5 }}</div>
+
+
+
+
+
+            </div>
+            <div class="col" v-for="item in info_4">
+                <h4>{{ item.title }}</h4>
+                <div>{{ item.text_1 }}</div>
+                <div>{{ item.text_2 }}</div>
+
+
+
+            </div>
+
+            <div class="col-8">
+                <img src="../assets/img/dc-logo-bg.png" alt="">
+
             </div>
         </div>
     </div>
@@ -28,5 +141,9 @@ export default {
 <style lang="scss" scoped>
 .container {
     background-color: rgb(217, 117, 41);
+
+    .row {
+        flex-wrap: nowrap;
+    }
 }
 </style>
--- src/components/AppLinks.vue
+++ src/components/AppLinks.vue
@@ -7,27 +7,27 @@ export default {
             links: [
                 {
                     text: 'DIGITAL COMICS',
-                    img: '../img/aquietplace.jpeg'
+                    img: '/src/assets/img/aquietplace.jpeg'
 
                 },
                 {
                     text: 'DC MERCHANDISE',
-                    img: '../img/aquietplace.jpeg'
+                    img: '/src/assets/img/aquietplace.jpeg'
 
                 },
                 {
                     text: 'SUBSCRIPTION',
-                    img: '../img/aquietplace.jpeg'
+                    img: '/src/assets/img/aquietplace.jpeg'
 
                 },
                 {
                     text: 'COMIC SHOP LOCATOR',
-                    img: '../img/aquietplace.jpeg'
+                    img: '/src/assets/img/aquietplace.jpeg'
 
                 },
                 {
                     text: 'DC POWER VISA',
-                    img: '../img/aquietplace.jpeg'
+                    img: '/src/assets/img/aquietplace.jpeg'
 
                 }
             ]
@@ -42,19 +42,39 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col" v-for="item in links">
-                <span>{{ item.img }} {{ item.text }}</span>
+    <div class="background">
+        <div class="container">
+            <div class="row">
+                <div class="col" v-for="item in links">
+                    <img :src="item.img">
+                    <span>{{ item.text }}</span>
+
+                </div>
             </div>
         </div>
+
     </div>
 </template>
 
 
 
 <style lang="scss" scoped>
+.background {
+    background-color: rgb(5, 80, 255);
+
+}
+
 .container {
-    background-color: rgb(165, 167, 33);
+
+
+
+    img {
+        height: 60px;
+    }
+
+    span {
+        font-size: 14px;
+        color: white;
+    }
 }
 </style>
